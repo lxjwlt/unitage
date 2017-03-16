68a17f0b74dd3b1e56ee270c61c85aa4e91f9798
lxjwlt <lxjwlt.c@gmail.com>
2017-03-16 14:02:02 +0800

    init unitage class

---
+++ src/index.js
@@ -0,0 +1,64 @@
+/**
+ * @file unitage class
+ */
+
+'use strict';
+
+class Unitage {
+
+    constructor (value, step, units) {
+        let self = this;
+
+        self.value = value;
+
+        self.step = step;
+
+        self.units = units;
+
+        self.tryMaxUnit();
+    }
+
+    tryMaxUnit (unit) {
+        let self = this;
+
+        let maxIndex = self.units.indexOf(unit);
+
+        maxIndex = maxIndex < 0 ? Infinity : maxIndex;
+
+        let result = specifyUnit(self.value, self.step, self.units, (value, index) => {
+            return index < maxIndex;
+        });
+
+        self.number = result.number;
+        self.unit = result.unit;
+    }
+
+    toString (unit) {}
+}
+
+function specifyUnit (value, step, units, goOn) {
+    let index = 0;
+
+    while (value >= step && index < units.length) {
+
+        if (goOn && goOn(value, index) === false) {
+            break;
+        }
+
+        value = value / step;
+        index += 1;
+    }
+
+    return {
+        number: value,
+        unit: units[index]
+    };
+}
+
+function creator () {
+    return new Unitage();
+}
+
+creator.Constructor = Unitage;
+
+module.exports = creator;
